1f7d8b8d87b7a7a0dd01bad05f4262b3daa35e2e
Ihor Zubenko <43724086+oawebdev@users.noreply.github.com>
2022-06-02 11:10:58 +0300

    token with brownie

---
+++ token/scripts/network_settings.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+#brownie networks add Ethereum ropsten host='https://speedy-nodes-nyc.moralis.io/7fb0a0413921c21a54e32cc1/eth/ropsten' chainid=3 explorer='https://api-ropsten.exit'
---
+++ token/scripts/polygon_setting.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+brownie networks add Polygon mumbai_moralis host='YOUR LINK' name='Mumbai (Moralis)' chainid=80001 explorer='https://explorer-mumbai.maticvigil.com/'
